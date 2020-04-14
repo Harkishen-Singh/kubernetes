cdb678ce643b9fe6a037deca591b24edb35f4979
needkane <e.zl@qq.com>
2020-04-14 00:05:35 -0400

    (return []error{} -> return nil) and (update annotation)
    improved logic. TODO: update unit-tests
    
    Signed-off-by: Harkishen Singh <harkishensingh@hotmail.com>
    
    updated unit-tests in jsonpath
    
    Signed-off-by: Harkishen Singh <harkishensingh@hotmail.com>

--- staging/src/k8s.io/client-go/util/jsonpath/jsonpath.go
+++ staging/src/k8s.io/client-go/util/jsonpath/jsonpath.go
@@ -233,8 +233,11 @@ func (j *JSONPath) evalIdentifier(input []reflect.Value, node *IdentifierNode) (
 	return results, nil
 }
 
-func (j *JSONPath) evalMap(input []reflect.Value) ([]reflect.Value, error) {
+func (j *JSONPath) evalMap(input []reflect.Value, hasAsterisk bool) ([]reflect.Value, error) {
 	result := []reflect.Value{}
+	if !hasAsterisk {
+		return nil, fmt.Errorf("invalid jsonpath for objects. Index should be an asterisk when applied over an object")
+	}
 
 	for _, value := range input {
 		value, isNil := template.Indirect(value)
@@ -262,7 +265,7 @@ func (j *JSONPath) evalArray(input []reflect.Value, node *ArrayNode) ([]reflect.
 		}
 		if value.Kind() != reflect.Array && value.Kind() != reflect.Slice {
 			if value.Kind() == reflect.Map {
-				return j.evalMap(input)
+				return j.evalMap(input, node.HasAsterisk)
 			}
 			return input, fmt.Errorf("%v is not array or slice", value.Type())
 		}
